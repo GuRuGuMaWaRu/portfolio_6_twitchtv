b52d7a10e1c055bae988876ceaa27ea70bcb02fd
peter <peter.k.nets@gmail.com>
2016-06-21 11:40:32 +0300

    Add sort buttons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './style.scss';
 import NavBar from './components/nav/NavBar';
 import Stream from './components/streams/Stream';
+import SortButtons from './components/elements/SortButtons';
 
 class Main extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class Main extends React.Component {
           </div>
         </div>
         <div className="wrapper">
+          <SortButtons />
           <Stream picture="Warcraft" name="Warcraft" status="Offline" />
           <Stream picture="Starcraft" name="Starcraft" status="Online" />
           <Stream picture="Total War" name="Total War" status="Pending" />
