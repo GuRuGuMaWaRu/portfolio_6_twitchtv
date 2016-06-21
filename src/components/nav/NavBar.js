aa1383f27644260891df11bddf471c9d9a170dde
peter <peter.k.nets@gmail.com>
2016-06-21 12:23:55 +0300

    Add new flex

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,12 +6,14 @@ class NavBar extends React.Component {
       return (
         <div className="wrapper my-navbar">
           <h1>TWITCH TV</h1>
-          <input className="my-navbar-input" type="text"></input>
-          <ul className="my-navbar-menu">
-            <NavItem label="All"/>
-            <NavItem label="Online"/>
-            <NavItem label="Offline"/>
-          </ul>
+          <div className="my-navbar-controls">
+            <input className="my-navbar-controls-input" type="text"></input>
+            <ul className="my-navbar-controls-menu">
+              <NavItem label="All"/>
+              <NavItem label="Online"/>
+              <NavItem label="Offline"/>
+            </ul>
+          </div>
         </div>
       )
     }
--- src/components/nav/NavItem.js
+++ src/components/nav/NavItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 class NavItem extends React.Component {
   render() {
     return (
-      <li className="my-navbar-menu-item">
+      <li className="my-navbar-controls-menu-item">
         <a href="#">{this.props.label}</a>
       </li>
     )
