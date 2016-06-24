2ad3c02bfa5fbe9e7dd40832f579a9e0947bb69e
peter <peter.k.nets@gmail.com>
2016-06-24 09:30:28 +0300

    Add FeaturedStream

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'whatwg-fetch';
 import './style.scss';
 import NavBar from './components/nav/NavBar';
 import Stream from './components/streams/Stream';
+import FeaturedStream from './components/streams/FeaturedStream';
 import SortButtons from './components/elements/SortButtons';
 
 class Main extends React.Component {
@@ -11,14 +12,17 @@ class Main extends React.Component {
     super(props);
     this.state = {
       streams: [],
-      streamsList: ''
+      ftStream: []
     };
   }
 
   componentWillMount() {
     $.getJSON('https://api.twitch.tv/kraken/streams?callback=?', data => {
-      // console.log(data.streams[0]);
       this.setState({streams: data.streams});
+    });
+    $.getJSON('https://api.twitch.tv/kraken/channels/freecodecamp?callback=?', data => {
+      console.log(data);
+      this.setState({ftStream: data});
     });
   }
 
@@ -40,7 +44,10 @@ class Main extends React.Component {
         <NavBar getData={this.getData} />
         <div className="my-feature">
           <div className="wrapper">
-            <Stream logo="FCC" name="FreeCodeCamp" status="Online" />
+            <FeaturedStream name={this.state.ftStream.display_name}
+                            logo={this.state.ftStream.logo}
+                            status={this.state.ftStream.status}
+                            url={this.state.ftStream.url}/>
           </div>
         </div>
         <div className="wrapper">
