9c95933a3c54c3f435aaecc40ef151956ef536b1
peter <peter.k.nets@gmail.com>
2016-06-25 15:44:26 +0300

    No FeaturedStream, move to preset streams, boooring...

--- src/components/streams/Stream.js
+++ src/components/streams/Stream.js
@@ -4,13 +4,22 @@ class Stream extends React.Component {
 
   render() {
     let dummyLogo = "https://dummyimage.com/300/ecf0e7/5c5457.jpg&text=0x3F",
-        logo = this.props.logo === undefined ? dummyLogo : this.props.logo;
+        logo = this.props.logo === undefined ? dummyLogo : this.props.logo,
+        status = '';
 
-    return (
-        <a className="my-stream" data-content={this.props.game} href={this.props.link} target="_blank">
-          <img src={logo} />
-        </a>
-    );
+    if (this.props.game === "Offline" || this.props.game === "Account closed") {
+      return (
+          <a className="my-stream offline" data-content={this.props.game} href="#">
+            <img src={logo} />
+          </a>
+      );
+    } else {
+      return (
+          <a className="my-stream" data-content={this.props.game} href={this.props.link} target="_blank">
+            <img src={logo} />
+          </a>
+      );
+    }
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -12,52 +12,70 @@ class Main extends React.Component {
     super(props);
     this.state = {
       streams: [],
-      ftStream: []
+      streams2: []
     };
   }
 
   componentWillMount() {
-    $.getJSON('https://api.twitch.tv/kraken/streams?callback=?', data => {
-      this.setState({streams: data.streams});
-    });
-    $.getJSON('https://api.twitch.tv/kraken/channels/freecodecamp?callback=?', data => {
-      console.log(data);
-      this.setState({ftStream: data});
+    // $.getJSON('https://api.twitch.tv/kraken/streams?callback=?', data => {
+    //   this.setState({streams: data.streams});
+    // });
+    let channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb",
+    "thomasballinger","noobs2ninjas","beohoff","brunofin","comster404","test_channel",
+    "cretetion","sheevergaming","TR7K","OgamingSC2","ESL_SC2"];
+
+    channels.map(channel => {
+      $.getJSON('https://api.twitch.tv/kraken/channels/' + channel + '?callback=?', data => {
+        let streams2 = this.state.streams2;
+
+        streams2.push(data);
+        this.setState({streams2});
+      }).done(() => {
+        // console.log(this.state.streams2);
+      });
     });
   }
 
   render() {
-    // console.log(this.state.streams)
     let dummyLogo = "https://dummyimage.com/300/ecf0e7/5c5457.jpg&text=0x3F",
-        streams = this.state.streams.map(function(stream) {
-          // console.log(stream.channel.logo);
-          return <Stream
-                  key={stream._id}
-                  logo={stream.channel.logo === null ? dummyLogo : stream.channel.logo}
-                  preview={stream.preview.medium}
-                  game={stream.game}
-                  link={stream.channel.url} />
+        channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb",
+        "thomasballinger","noobs2ninjas","beohoff","brunofin","comster404","test_channel",
+        "cretetion","sheevergaming","TR7K","OgamingSC2","ESL_SC2"],
+        streams2 = this.state.streams2.map(function(stream, key) {
+          return <Stream key={key} logo={stream.logo} game={stream.game} link={stream.url}/>;
         });
+
+
+        // streams = this.state.streams.map(function(stream) {
+        //   let status = '';
+        //   if (stream.channel === null) {
+        //     status = "Offline";
+        //   } else if (stream.channel === undefined) {
+        //     status = "Account closed";
+        //   } else {
+        //     status = stream.game;
+        //   }
+        //   return <Stream
+        //           key={stream._id}
+        //           logo={stream.channel.logo === null ? dummyLogo : stream.channel.logo}
+        //           game={status}
+        //           link={stream.channel.url} />
+        // });
+
 
     return (
       <div>
         <NavBar getData={this.getData} />
         <div className="my-feature">
-          <div className="wrapper">
-            <FeaturedStream name={this.state.ftStream.display_name}
-                            logo={this.state.ftStream.logo}
-                            status={this.state.ftStream.status}
-                            url={this.state.ftStream.url}/>
-          </div>
         </div>
         <div className="wrapper">
           <SortButtons />
           <div className="my-stream-list">
-            {streams}
+            {streams2}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
